cc5ccd1f3c92fc97e65cecfffb1119a70eab929e
thomasstrassmann <thomas_strassmann@freenet.de>
2022-12-14 14:33:55 +0000

    add searchbar

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -10,6 +10,7 @@ export class Content extends Component {
     
       this.state = {
         isLoaded: false,
+        posts: [],
       }
     }
 
@@ -17,10 +18,20 @@ export class Content extends Component {
       setTimeout(()=>{
           this.setState({
               isLoaded: true,
+              posts: savedPosts,
           })
       }, 2000)
   }
-  
+
+    handleChange = (event) =>{
+        let name = event.target.value.toLowerCase();
+        const filteredPosts = savedPosts.filter((post)=> {
+            return post.name.toLowerCase().includes(name);
+        })
+        this.setState({
+            posts: filteredPosts,
+        })
+    }
 
     render() {
       return (
@@ -28,12 +39,18 @@ export class Content extends Component {
               
               <div className={css.TitleBar}>
                   <h1>My Photos</h1>
+                  <form>
+                    <label htmlFor='searchInput'>Search:</label>
+                    <input type="text" id="searchInput" name="input" 
+                    placeholder='By Author...' onChange={(event) => this.handleChange(event)}></input>
+                    <h4>posts found: {this.state.posts.length}</h4>
+                  </form>
               </div>
 
               <div className={css.SearchResults}>
                   {
                       this.state.isLoaded ?
-                      <PostItem savedPosts={savedPosts} />
+                      <PostItem savedPosts={this.state.posts} />
                       : <Loader />
                   }
               </div>
