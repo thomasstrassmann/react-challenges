3d71f9683c73f236677daf7561ae3e8ae0bcf443
thomasstrassmann <thomas_strassmann@freenet.de>
2022-12-15 15:44:42 +0000

    work with API

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,17 @@
 import css from './App.module.css';
 // import Content from './components/Content';
-// import Sidebar from './components/Sidebar';
+import Sidebar from './components/Sidebar';
 // import NavBarSimple from './components/NavBarSimple';
-import ContentHooks from './components/ContentHooks';
+// import ContentHooks from './components/ContentHooks';
 import NavBarForm from './components/NavBarForm';
+import ContentAPIHooks from './components/ContentAPIHooks';
 
 function App() {
   return (
     <div className={css.App}>
       <NavBarForm />
-      <ContentHooks/>
+      <Sidebar/>
+      <ContentAPIHooks/>
     </div>
   );
 }
